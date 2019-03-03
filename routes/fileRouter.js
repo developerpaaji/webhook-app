65c40045685c5b05648d57f50bfc8bddba03c4a2
bhavneet <bhavneets46@gmail.com>
2019-03-03 21:46:14 +0530

    webhook

--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 /* Webhook testing*/
 router.get('/update', function(req, res, next) {
+  var challenge=req.params.challenge;
   console.log(req.body);
-  res.send('nxIKj-ZRT1TEoCvqe6d1AkWdaZU4L5fGpgI16lxGBI4');
+  res.send(challenge);
 });
 /* Webhook posting */
 router.post('/update', function(req, res, next) {
