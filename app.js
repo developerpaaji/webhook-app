14720f495bc01b36d0f293ceef742b3a19183720
bhavneet <bhavneets46@gmail.com>
2019-03-03 21:34:07 +0530

    webhook

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const File=require(path.join(__dirname,'controllers','fileDatabase.js'));  
 var indexRouter = require('./routes/index');
-var postRouter = require('./routes/fileRouter');
+var fileRouter = require('./routes/fileRouter');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/post', postRouter);
+app.use('/file', fileRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
