5ce5fac5eb762541e5b32550bddfd3390107946c
bhavneet <bhavneets46@gmail.com>
2019-03-03 21:33:11 +0530

    webhook

--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/update', function(req, res, next) {
+router.post('/update', function(req, res, next) {
   console.log(req.body);
   res.send('respond with a resource');
 });
